151f04688ac850b9e21c8140a112d3ca19e24c1b
Spyron1234 <stack.surya@gmail.com>
2024-10-02 22:43:35 +0530

    rabit health disabled, logging

--- src/main/java/com/os/app/OrderController.java
+++ src/main/java/com/os/app/OrderController.java
@@ -1,5 +1,7 @@
 package com.os.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order/")
 public class OrderController {
 	
+	Logger logger = LoggerFactory.getLogger(OrderController.class);
+	
 	@Autowired
 	LocationConfig locationConfig;
 
 	@GetMapping("/loc")
 	public String details() {
+		logger.info("Order Location Trace Request Reached..........");
 		return locationConfig.getName() != null ? locationConfig.getName() : "KIOL";
 	}
 }
